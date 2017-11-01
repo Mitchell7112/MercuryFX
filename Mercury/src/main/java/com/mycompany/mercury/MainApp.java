350563891a76fb7d2e4a63dc3bc40a1a6a5f1cf8
Mitchell7112 <Mitchellwan28@gmail.com>
2017-11-01 13:46:18 +0100

    Added assets

--- Mercury/src/main/java/com/mycompany/mercury/MainApp.java
+++ Mercury/src/main/java/com/mycompany/mercury/MainApp.java
@@ -17,7 +17,8 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Mercury");
+        stage.setFullScreen(true);
         stage.setScene(scene);
         stage.show();
     }
